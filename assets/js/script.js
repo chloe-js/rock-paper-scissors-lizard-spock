b65dc38db1376662aa7d56b620b8067be23c54a4
chloë js <chloejs.horta@gmail.com>
2022-02-14 22:05:13 +0000

    active difficulty set

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,13 @@ btnDifficulty.addEventListener('click', toggleDifficulty)
 function toggleDifficulty() {
     console.log(activateDifficulty);
     activateDifficulty = !activateDifficulty;
-    document.getElementById('level').innerText = 1;
+    
+    if (activateDifficulty){
+            document.getElementById('level').innerText = 'HARD!';
+        } else if (!activateDifficulty){
+            document.getElementById('level').innerText = 'EASY';
+        }
+        
     console.log(activateDifficulty);
 }
 // // winning hands values
@@ -29,7 +35,7 @@ const weakness = {
     Lizard: ['Scissors', 'Rock'],
     Spock: ['Paper', 'Lizard']
 }
-console.log(weakness.Paper)
+
 // // rules for who wins what hand
 const rules = [
     ['Scissors', 'Paper'],
@@ -48,22 +54,19 @@ const rules = [
 function checkResult(res) {
 
     if (activateDifficulty) {
-        console.log('testing true')
-        console.log(weakness[res], ' --access')
+            console.log('testing true')
+            console.log(weakness[res], ' --access')
         opponent.push(weakness[res][0])
-        document.getElementById('level').innerText = 0;
         opponent.push(weakness[res][1])
-        document.getElementById('level').innerText = 0;
-        console.log(activateDifficulty)
-        console.log(opponent)
-
-    } console.log(activateDifficulty)
+            console.log(activateDifficulty)
+            console.log(opponent)
+    } 
+    console.log(activateDifficulty)
     console.log(opponent)
 
-    const opponentHand = opponent[Math.floor(Math.random() * opponent.length)];
-
-    let result = false
-    let draw = false;
+const opponentHand = opponent[Math.floor(Math.random() * opponent.length)];
+let result = false
+let draw = false;
 
     for (var i = 0; i < rules.length; i++) {
         if (res === rules[i][0] && opponentHand === rules[i][1]) {
@@ -72,6 +75,8 @@ function checkResult(res) {
             draw = true;
         }
     }
+
+    
 // // if win get point if loose no point and draw no point
     if (result && !draw) {
         alert(`${res} beats ${opponentHand} -- YOU WIN!`);
@@ -82,7 +87,20 @@ function checkResult(res) {
     } else if (!result && !draw) {
         alert(`${res} loses to ${opponentHand} -- YOU LOSE!`);
         player2Score.innerText = String(++p2Score);
+        
+    } 
+    
+function firstToFive() {
+    if ( p1Score.length < 5) {
+        alert(`CONGRATUALTIONS PLAYER 1 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
+        console.log(firstToFive)
+        console.log(p1Score.length)
+    } else if (p2Score.length <5 ) {
+        alert(`YOU LOOSE! PLAYER 2 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
+        console.log(firstToFive)
+        console.log(p2Score.length)
     }
+}
 // // after hand played, difficulty removed
     if (activateDifficulty) {
         console.log('testing if still true')
@@ -102,143 +120,9 @@ document.getElementById('resetButton').addEventListener("click", function () {
     p2Score = 0;
     player2Score.innerText = 0;
 }); 
-// // btnDifficulty.addEventListener("click", function() {
-if (activateDifficulty){
-    document.getElementById('level').innerText = 1;
-} else if (!activateDifficulty){
-    document.getElementById('level').innerText = 0;
-}
 
 
-// btnDifficulty.addEventListener("click", function() {
-//     if (activateDifficulty){
-//         document.getElementById('level').innerText = 1;
-//     } else if (!activateDifficulty){
-//         document.getElementById('level').innerText = 0;
-//     }
-//   });
-
-
-// let p1Score = 0
-// let p2Score = 0
-// let activateDifficulty = false
-// let playerChoice = document.querySelectorAll('.playerChoice');
-// let btnDifficulty = document.getElementById('difficulty')
-// const player1Score = document.getElementById('scoresPlayer1');
-// const player2Score = document.getElementById('scoresPlayer2');
-// const opponent = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
-
-// // click event listener for player choice
-// playerChoice.forEach(i => i.addEventListener('click', function () {
-//     // checkResult(this.value);
-//     console.log(this.value)
-// }))
-// btnDifficulty.addEventListener('click', toggleDifficulty)
-
-// // funtion for turn on Difficulty
-// function toggleDifficulty() {
-//     console.log(activateDifficulty);
-//     activateDifficulty = !activateDifficulty;
-//     document.getElementById('level').innerText = 1;
-//     console.log(activateDifficulty);
-// }
-// // winning hands values
-// const weakness = {
-//     Rock: ['Paper', 'Spock'],
-//     Paper: ['Lizard', 'Scissors'],
-//     Scissors: ['Rock', 'Spock'],
-//     Lizard: ['Scissors', 'Rock'],
-//     Spock: ['Paper', 'Lizard']
-// }
-// console.log(weakness.Paper)
-// // rules for who wins what hand
-// const rules = [
-//     ['Scissors', 'Paper'],
-//     ['Paper', 'Rock'],
-//     ['Rock', 'Lizard'],
-//     ['Lizard', 'Spock'],
-//     ['Spock', 'Scissors'],
-//     ['Scissors', 'Lizard'],
-//     ['Lizard', 'Paper'],
-//     ['Paper', 'Spock'],
-//     ['Spock', 'Rock'],
-//     ['Rock', 'Scissors']
-// ]
-// // difficulty is on give computer extra winning hands from weakness
-// function checkResult(res) {
-
-//     if (activateDifficulty) {
-//         // console.log('testing true')
-//         // console.log(weakness[res], ' --access')
-//         opponent.push(weakness[res][0])
-//         // document.getElementById('level').innerText = 0;
-//         opponent.push(weakness[res][1])
-//         // document.getElementById('level').innerText = 0;
-//         // console.log(activateDifficulty)
-//         // console.log(opponent)
-
-//     } 
-//     // console.log(activateDifficulty)
-//     // console.log(opponent)
-
-// const opponentHand = opponent[Math.floor(Math.random() * opponent.length)];
-// let result = false
-// let draw = false;
-
-//     for (var i = 0; i < rules.length; i++) {
-//         if (res === rules[i][0] && opponentHand === rules[i][1]) {
-//             result = true;
-//         } else if (res === opponentHand) {
-//             draw = true;
-//         }
-//     }
-// // if win get point if loose no point and draw no point
-//     if (result && !draw) {
-//         alert(`${res} beats ${opponentHand} -- YOU WIN!`);
-//         player1Score.innerText = String(++p1Score); 
-//         console.log(p1Score)
-//     } else if (!result && draw) {
-//         alert(`DRAW: ${res} vs ${opponentHand}`);
-//     } else if (!result && !draw) {
-//         alert(`${res} loses to ${opponentHand} -- YOU LOSE!`);
-//         player2Score.innerText = String(++p2Score);
-//     }
-// // after hand played, difficulty removed
-//     if (activateDifficulty) {
-//         // console.log('testing if still true')
-//         // console.log(activateDifficulty, ' --accessing')
-//         // console.log(opponent, '---this is the arry after')
-//         // console.log(opponent)
-//             opponent.pop(weakness[0]);
-//         // console.log(opponent.pop(weakness[0]), ' --after removed ')
-//         // console.log(activateDifficulty)
-//         // console.log(opponent, '---this is the after pop')
-//     } 
-//     // console.log(activateDifficulty)
-// }
-// // reset button clean score and starts again
-// document.getElementById('resetButton').addEventListener("click", function () {
-//     p1Score = 0;
-//     player1Score.innerText = 0;
-//     p2Score = 0;
-//     player2Score.innerText = 0;
-// }); 
-// // Difficult score displayed
-// if (activateDifficulty){
-//     document.getElementById('level').innerText = 1;
-// } else if (!activateDifficulty){
-//     document.getElementById('level').innerText = 0;
-// }
-
-
-// // btnDifficulty.addEventListener("click", function() {
-// //     if (activateDifficulty){
-// //         document.getElementById('level').innerText = 1;
-// //     } else if (!activateDifficulty){
-// //         document.getElementById('level').innerText = 0;
-// //     }
-// //   });
-
+    
 // //////////////////////PIZZA PIZZA
 // // function createSelectedToppingElement(toppings) {
 // //     // if(selectedToppings.children.length !== 5)
