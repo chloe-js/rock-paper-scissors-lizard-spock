3723abb5ae1ea66d783c482fbb7a119010ffefdb
chloë js <chloejs.horta@gmail.com>
2022-02-15 23:26:37 +0000

    formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,13 +20,13 @@ console.log(playerChoice)
 function toggleDifficulty() {
     console.log(activateDifficulty);
     activateDifficulty = !activateDifficulty;
-    
-    if (activateDifficulty){
-            document.getElementById('level').innerText = ' HARD! ';
-        } else if (!activateDifficulty){
-            document.getElementById('level').innerText = ' EASY ';
-        }
-        
+
+    if (activateDifficulty) {
+        document.getElementById('level').innerText = ' HARD! ';
+    } else if (!activateDifficulty) {
+        document.getElementById('level').innerText = ' EASY ';
+    }
+
     console.log(activateDifficulty);
 }
 // // winning hands values
@@ -56,19 +56,19 @@ const rules = [
 function checkResult(res) {
 
     if (activateDifficulty) {
-            console.log('testing true')
-            console.log(weakness[res], ' --access')
+        console.log('testing true')
+        console.log(weakness[res], ' --access')
         opponent.push(weakness[res][0])
         opponent.push(weakness[res][1])
-            console.log(activateDifficulty)
-            console.log(opponent)
-    } 
+        console.log(activateDifficulty)
+        console.log(opponent)
+    }
     console.log(activateDifficulty)
     console.log(opponent)
 
-const opponentHand = opponent[Math.floor(Math.random() * opponent.length)];
-let result = false
-let draw = false;
+    const opponentHand = opponent[Math.floor(Math.random() * opponent.length)];
+    let result = false
+    let draw = false;
 
     for (var i = 0; i < rules.length; i++) {
         if (res === rules[i][0] && opponentHand === rules[i][1]) {
@@ -79,57 +79,58 @@ let draw = false;
     }
 
 
-// // if win get point if loose no point and draw no point
+    // // if win get point if loose no point and draw no point
 
-    if (result && !draw) {       
+    if (result && !draw) {
         alert(`${res} beats ${opponentHand} -- YOU WIN!`);
-        player1Score.innerText = String(++p1Score); 
+        player1Score.innerText = String(++p1Score);
         console.log(p1Score)
-        winnerAlertPopUp()//////////////////////////////////////////////////////////////////////
+        winnerAlertPopUp() //////////////////////////////////////////////////////////////////////
     } else if (!result && draw) {
         alert(`DRAW: ${res} vs ${opponentHand}`);
     } else if (!result && !draw) {
         alert(`${res} loses to ${opponentHand} -- YOU LOSE!`);
         player2Score.innerText = String(++p2Score);
         firstToFive();
-    } 
-
-///////////////////////////POTENTIAL WINNER ALERT POPUP WHEN eins hand////////////////////////////////////////      
-
-function winnerAlertPopUp(){
-    const alertElement = document.createElement('span');
-    alertElement.setAttribute('class', 'popup-alert');
-    alertElement.setAttribute('id', 'winner-alert')
-    console.dir(alertElement)
-    alertElement.innerText = `CONGRATUALTIONS YOU WON ${res}`;
-    document.body.appendChild(alertElement);
-    setTimeout(function () {
-        document.getElementById('winner-alert').remove();
-    }, 5000)
-}
-///////////////////////////POTENTIAL WINNER SCORE WHEN HIT 5 in a row////////////////////////////////////////  
-function firstToFive() {
-    if ( p1Score === 5) {
-        alert(`CONGRATUALTIONS PLAYER 1 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
-        console.log(firstToFive)
-        console.log(p1Score.length)
-    } else if (p2Score === 5 ) {
-        alert(`YOU LOOSE! PLAYER 2 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
-        console.log(firstToFive)
-        console.log(p2Score.length)
     }
-}
-// // after hand played, difficulty removed
+
+    ///////////////////////////POTENTIAL WINNER ALERT POPUP WHEN eins hand////////////////////////////////////////      
+
+    function winnerAlertPopUp() {
+        const alertElement = document.createElement('span');
+        alertElement.setAttribute('class', 'popup-alert');
+        alertElement.setAttribute('id', 'winner-alert')
+        console.dir(alertElement)
+        alertElement.innerText = `CONGRATUALTIONS YOU WON ${res}`;
+        document.body.appendChild(alertElement);
+        setTimeout(function () {
+            document.getElementById('winner-alert').remove();
+        }, 5000)
+    }
+    ///////////////////////////POTENTIAL WINNER SCORE WHEN HIT 5 in a row////////////////////////////////////////  
+    function firstToFive() {
+        if (p1Score === 5) {
+            alert(`CONGRATUALTIONS PLAYER 1 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
+            console.log(firstToFive)
+            console.log(p1Score.length)
+        } else if (p2Score === 5) {
+            alert(`YOU LOOSE! PLAYER 2 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
+            console.log(firstToFive)
+            console.log(p2Score.length)
+        }
+    }
+    // // after hand played, difficulty removed
     if (activateDifficulty) {
         console.log('testing if still true')
         console.log(activateDifficulty, ' --accessing')
         console.log(opponent, '---this is the arry after')
         console.log(opponent)
-            opponent.pop(weakness[0]);
+        opponent.pop(weakness[0]);
         console.log(opponent.pop(weakness[0]), ' --after removed ')
         console.log(activateDifficulty)
         console.log(opponent, '---this is the after pop')
-    } console.log(activateDifficulty)
+    }
+    console.log(activateDifficulty)
 }
 // // reset button clean score and starts again
 document.getElementById('resetButton').addEventListener("click", function () {
@@ -137,10 +138,10 @@ document.getElementById('resetButton').addEventListener("click", function () {
     player1Score.innerText = 0;
     p2Score = 0;
     player2Score.innerText = 0;
-}); 
+});
 
 
-    
+
 // //////////////////////PIZZA PIZZA
 // // function createSelectedToppingElement(toppings) {
 // //     // if(selectedToppings.children.length !== 5)
