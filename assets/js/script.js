7ef495cf94f02727a32c56b40a97f858ec4041b7
chloë js <chloejs.horta@gmail.com>
2022-02-17 23:50:13 +0000

    link script and id elements to new id and classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,29 @@
-let p1Score = 0
-let p2Score = 0
-let activateDifficulty = false
+let p1Score = 0;
+let p2Score = 0;
+let activateDifficulty = false;
 let playerChoice = document.body.querySelectorAll('.player-control');
 let btnDifficulty = document.getElementById('difficulty');
 const player1Score = document.getElementById('player-1-score');
 const player2Score = document.getElementById('player-2-score');
 const opponent = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
 
-console.log(playerChoice)
 // // click event listener for player choice
 playerChoice.forEach(i => i.addEventListener('click', function () {
     checkResult(this.value);
-    console.log(this.value)
-}))
-btnDifficulty.addEventListener('click', toggleDifficulty)
-console.log(playerChoice)
-// // funtion for turn on Difficulty
-function toggleDifficulty() {
-    console.log(activateDifficulty);
-    activateDifficulty = !activateDifficulty;
+}));
 
+// // funtion for turn on Difficulty
+btnDifficulty.addEventListener('click', toggleDifficulty);
+
+function toggleDifficulty() {
+    activateDifficulty = !activateDifficulty;
     if (activateDifficulty) {
         document.getElementById('difficulty-ref').innerText = 'hard!';
     } else if (!activateDifficulty) {
         document.getElementById('difficulty-ref').innerText = 'easy';
     }
-
-    console.log(activateDifficulty);
 }
+
 // // winning hands values
 const weakness = {
     Rock: ['Paper', 'Spock'],
@@ -55,18 +51,12 @@ const rules = [
 function checkResult(res) {
 
     if (activateDifficulty) {
-        console.log('testing true')
-        console.log(weakness[res], ' --access')
-        opponent.push(weakness[res][0])
-        opponent.push(weakness[res][1])
-        console.log(activateDifficulty)
-        console.log(opponent)
+        opponent.push(weakness[res][0]);
+        opponent.push(weakness[res][1]);
     }
-    console.log(activateDifficulty)
-    console.log(opponent)
 
     const opponentHand = opponent[Math.floor(Math.random() * opponent.length)];
-    let result = false
+    let result = false;
     let draw = false;
 
     for (var i = 0; i < rules.length; i++) {
@@ -77,128 +67,83 @@ function checkResult(res) {
         }
     }
 
-
     // // if win get point if loose no point and draw no point
-
     if (result && !draw) {
-        // alert(`${res} beats ${opponentHand} -- YOU WIN!`);
         player1Score.innerText = String(++p1Score);
-        console.log(p1Score)
-        winnerAlertPopUp()
-        firstToFive() //////////////////////////////////////////////////////////////////////
+        perRoundAlertPopUp('win');
+        firstToFive();
     } else if (!result && draw) {
-        drawAlertPopUp()
-        // alert(`DRAW: ${res} vs ${opponentHand}`);
+        perRoundAlertPopUp('draw');
     } else if (!result && !draw) {
-        // alert(`${res} loses to ${opponentHand} -- YOU LOSE!`);
         player2Score.innerText = String(++p2Score);
-        loserAlertPopUp()
+        perRoundAlertPopUp('loss');
         firstToFive();
     }
 
-    ///////////////////////////POTENTIAL WINNER ALERT POPUP WHEN eins hand////////////////////////////////////////      
-
-    function winnerAlertPopUp() {
+    function perRoundAlertPopUp(result) {
         const alertElement = document.createElement('span');
         alertElement.setAttribute('class', 'popup-alert');
-        alertElement.setAttribute('id', 'winner-alert')
-        console.dir(alertElement)
-        alertElement.innerText = `CONGRATUALTIONS YOU WON! ${res} BEATS ${opponentHand}`;
+        alertElement.setAttribute('id', 'per-round-alert-popup');
+        if (result === 'win') {
+            alertElement.innerText = `CONGRATUALTIONS YOU WON! ${res} BEATS ${opponentHand}`;
+        } else if (result === 'draw') {
+            alertElement.innerText = `DRAW ${res} VS ${opponentHand}`;
+        } else if (result === 'loss') {
+            alertElement.innerText = `LOSER! ${opponentHand} BEATS ${res}`;
+        }
         document.body.appendChild(alertElement);
         setTimeout(function () {
-            document.getElementById('winner-alert').remove();
-        }, 1000)
+            document.getElementById('per-round-alert-popup').remove();
+        }, 7000);
     }
 
-    function loserAlertPopUp() {
-        const alertElement = document.createElement('span');
-        alertElement.setAttribute('class', 'popup-alert');
-        alertElement.setAttribute('id', 'loser-alert')
-        console.dir(alertElement)
-        alertElement.innerText = `UNFORTUNATELY YOU LOST! ${opponentHand} BEATS ${res}`;
-        document.body.appendChild(alertElement);
-        setTimeout(function () {
-            document.getElementById('loser-alert').remove();
-        }, 1000)
-    }
-
-    function drawAlertPopUp() {
-        const alertElement = document.createElement('span');
-        alertElement.setAttribute('class', 'popup-alert');
-        alertElement.setAttribute('id', 'draw-alert')
-        console.dir(alertElement)
-        alertElement.innerText = `ITS A DRAW! TRY AGAIN!`;
-        document.body.appendChild(alertElement);
-        setTimeout(function () {
-            document.getElementById('draw-alert').remove();
-        }, 1000)
-    }
-    ///////////////////////////POTENTIAL WINNER SCORE WHEN HIT 5 in a row////////////////////////////////////////  
     function firstToFive() {
         if (p1Score === 5) {
-            // alert(`CONGRATUALTIONS PLAYER 1 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
-            championAlertPopUp()
-            console.log(firstToFive)
-            console.log(p1Score.length)
+            championAlertPopUp();
         } else if (p2Score === 5) {
-            // alert(`YOU LOOSE! PLAYER 2 YOU WON! YOUR SCORE: ${p1Score} PLAYER 2 SCORE: ${p2Score}`);
-            tryAgainAlertPopUp()
-            console.log(firstToFive)
-            console.log(p2Score.length)
+            tryAgainAlertPopUp();
         }
     }
 
     function championAlertPopUp() {
         const alertElement = document.createElement('span');
         alertElement.setAttribute('class', 'popup-alert');
-        alertElement.setAttribute('id', 'champion-alert')
-        console.dir(alertElement)
+        alertElement.setAttribute('id', 'champion-alert');
         alertElement.innerText = `CONGRATULATIONS ! YOU ARE THE CHAMPION ! YOUR SCORE: ${p1Score} OPPONENT SCORE: ${p2Score} `;
         document.body.appendChild(alertElement);
         setTimeout(function () {
             document.getElementById('champion-alert').remove();
-        }, 7000)
+        }, 7000);
         p1Score = 0;
-        player1Score.innerText = 0;
+        player1Score.innerText = String(0);
         p2Score = 0;
-        player2Score.innerText = 0;
+        player2Score.innerText = String(0);
     }
 
     function tryAgainAlertPopUp() {
         const alertElement = document.createElement('span');
         alertElement.setAttribute('class', 'popup-alert');
-        alertElement.setAttribute('id', 'tryAgain-alert')
-        console.dir(alertElement)
+        alertElement.setAttribute('id', 'tryAgain-alert');
         alertElement.innerText = `YOU LOOSE! YOUR SCORE: ${p1Score} OPPONENT SCORE: ${p2Score}`;
         document.body.appendChild(alertElement);
         setTimeout(function () {
             document.getElementById('tryAgain-alert').remove();
-        }, 7000)
+        }, 7000);
         p1Score = 0;
-        player1Score.innerText = 0;
+        player1Score.innerText = String(0);
         p2Score = 0;
-        player2Score.innerText = 0;
+        player2Score.innerText = String(0);
     }
     // // after hand played, difficulty removed
     if (activateDifficulty) {
-        console.log('testing if still true')
-        console.log(activateDifficulty, ' --accessing')
-        console.log(opponent, '---this is the arry after')
-        console.log(opponent)
-
         opponent.pop();
         opponent.pop();
-
-        console.log(opponent.pop, ' --after removed ')
-        console.log(activateDifficulty)
-        console.log(opponent, '---this is the after pop')
     }
-    console.log(activateDifficulty)
 }
 // // reset button clean score and starts again
 document.getElementById('reset-button').addEventListener("click", function () {
     p1Score = 0;
-    player1Score.innerText = 0;
+    player1Score.innerText = String(0);
     p2Score = 0;
-    player2Score.innerText = 0;
+    player2Score.innerText = String(0);
 });
